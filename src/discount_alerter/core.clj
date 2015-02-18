ea9b9d73a1ee2b68188ab688c954bc9e1ccd51ad
Ali Sarac <alisarac@gmail.com>
2015-02-19 01:35:51 +0200

    Async page download is added

--- src/discount_alerter/core.clj
+++ src/discount_alerter/core.clj
@@ -10,6 +10,18 @@
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
+(defn fetch-url-sync [url]
+  (let [{:keys [status headers body error] :as resp} @(http/get url)]
+    (when-not error
+      (html/html-resource (java.io.StringReader. body)))))
+
+(defn fetch-urls-async [urls]
+  (let [futures (doall (map http/get urls))]
+    (for [resp futures
+          :let [{:keys [body error] :as r} @resp]
+          :when (not error)]
+      (html/html-resource (java.io.StringReader. body)))))
+
 (defn get-name [pg]
   (first (:content (first (html/select pg [:div.apphub_AppName])))))
 
@@ -19,13 +31,22 @@
                                 :div.discount_final_price]))))
 
 (defn get-game [id]
-  (let [pg (fetch-url (str base-url id))
+  (let [pg (fetch-url-sync (str base-url id))
         name (get-name pg)
         discounted? (discounted? pg)]
     {:id id :name name :discounted? discounted?}))
 
+(defn get-games [pages]
+  (for [pg (fetch-urls-async pages)
+        :let [name (get-name pg)
+              discounted? (discounted? pg)]]
+    {:name name :discounted? discounted?}))
+
 (defn discount-report [g]
   (mapv get-game g))
+
+(defn discount-reports [g]
+  (get-games (mapv #(str base-url %) g)))
 
 (defn -main
   "Get discount reports of games in argument list"
